223f7481114887a43632517d86227c3905310693
Suman Komarla Adinarayana <67361562+suman-k-adinarayana@users.noreply.github.com>
2024-11-14 22:10:31 +0530

    Add files via upload
    Functions

---
+++ SumanKA_CLibraryInformation_Function.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+
+// Create a function
+void ska_Function() {
+  printf("\n I am in a function! A \"C function\"! :) \n");
+}
+
+int main() {
+  ska_Function(); // call the function
+  getch();
+  return 0;
+
+}
